5595ee3d89db0b9d82e8a676ac2c1b4140a0c394
GabrielF-C <45073321+GabrielF-C@users.noreply.github.com>
2019-03-29 10:29:02 -0400

    Update Questionnaire.cs

--- TP2_Base_de_données/Custom Controls/Questionnaire.cs
+++ TP2_Base_de_données/Custom Controls/Questionnaire.cs
@@ -14,7 +14,10 @@ namespace TP2_Base_de_données.Custom_Controls
     public partial class Questionnaire : UserControl
     {
         private Question _Question;
-
+        
+        /// <summary>La question à laquelle l'utilisateur devra répondre</summary>
+        /// <remark>Changer la valeur de cette propriété mettra le questionnaire à jour automatiquement</remark>
+        public Question ARepondre { get => return _Question; set => SetQuestion(value); }
         /// <summary>La réponse choisie par l'utilisateur</summary>
         public Reponse Choisie { get; private set; }
         /// <summary>Se produit quand le bouton Répondre est cliqué</summary>
@@ -25,29 +28,30 @@ namespace TP2_Base_de_données.Custom_Controls
             InitializeComponent();
         }
 
-        public Questionnaire(Question aRepondre)
-        {
-            InitializeComponent();
-
-            _Question = aRepondre;
-        }
         private void Questionnaire_Load(object sender, EventArgs e)
         {
             Choisie = null;
-
-            if (_Question != null)
-            {
-                InitQuestionnaire();
-            }
+        }
+        
+        private void SetQuestion(Question value)
+        {
+            _Question = value;
+            InitQuestionnaire();
         }
 
         private void InitQuestionnaire()
         {
+            Choisie = null;
+            BTN_Repondre.Enabled = false;
             RTBX_Question.Text = _Question.Enonce;
             RTBX_A_Reponse.Text = _Question.Reponses[0].Description;
             RTBX_B_Reponse.Text = _Question.Reponses[1].Description;
             RTBX_C_Reponse.Text = _Question.Reponses[2].Description;
             RTBX_D_Reponse.Text = _Question.Reponses[3].Description;
+            RBTN_A_Reponse.Checked = false;
+            RBTN_B_Reponse.Checked = false;
+            RBTN_C_Reponse.Checked = false;
+            RBTN_D_Reponse.Checked = false;
         }
 
         private void RBTN_Reponse_CheckedChanged(object sender, EventArgs e)
