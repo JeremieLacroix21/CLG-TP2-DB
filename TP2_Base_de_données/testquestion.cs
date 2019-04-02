a15600dbe0e36db8bba50076ee1d489b995047f3
JeremieLacroix21 <Jrmie@DESKTOP-JL90G99>
2019-04-02 19:05:33 -0400

    ok

---
+++ TP2_Base_de_données/testquestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP2_Base_de_données
+{
+    public partial class testquestion : Form
+    {
+        public testquestion()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
